0be3395b94c383e6b12b323a9e658bb2cbc776c1
ananyaprabhakarm <ananyaprabhakarm@gmail.com>
2024-09-22 12:39:56 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ));
+
+    return Scaffold(
+        backgroundColor: Colors.black,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarBrightness: Brightness.dark,
+          ),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 0),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: AlignmentGeometry(3,-0.3),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: ,
+                    )
+                  )
+              ]
+            )
+          ),
+        ));
+  }
+}
